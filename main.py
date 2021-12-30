b9dc6f7bd584282f9383b241d8bf27799ac46a12
Vagelis Prokopiou <vagelis.prokopiou@gmail.com>
2021-12-30 16:14:44 +0200

    Done

--- main.py
+++ main.py
@@ -1,39 +1,18 @@
-# Run: uvicorn main:app --host 127.0.0.1 --port 8080
+# Run: uvicorn main:app --host 127.0.0.1 --port 8000
 
 import json
-from json.encoder import JSONEncoder
 import falcon
 import falcon.asgi
-
-
-# Falcon follows the REST architectural style, meaning (among
-# other things) that you think in terms of resources and state
-# transitions, which map to HTTP verbs.
-class ThingsResource:
-    async def on_get(self, req, resp):
-        """Handles GET requests"""
-        resp.status = falcon.HTTP_200  # This is the default status
-        resp.content_type = falcon.MEDIA_TEXT  # Default is JSON, so override
-        resp.text = ('\nTwo things awe me most, the starry sky '
-                     'above me and the moral law within me.\n'
-                     '\n'
-                     '    ~ Immanuel Kant\n\n')
 
 
 # falcon.asgi.App instances are callable ASGI apps...
 # in larger applications the app is created in a separate file
 app = falcon.asgi.App()
 
-# Resources are represented by long-lived class instances
-things = ThingsResource()
-
-# things will handle all requests to the '/things' URL path
-app.add_route('/things', things)
-
 
 def getUsers():
     users = []
-    for index in range(1, 10):
+    for index in range(1, 1001):
         strIndex = str(index)
         firstName = "FirstName" + strIndex
         lastName = "LastName" + strIndex
@@ -50,16 +29,11 @@ def getUsers():
 
 class Users:
     async def on_get(self, req, resp):
-        """Handles GET requests"""
-        resp.status = falcon.HTTP_200  # This is the default status
-        # resp.content_type = falcon.MEDIA_TEXT  # Default is JSON, so override
-        resp.text = 'some text'
+        resp.body = json.dumps(getUsers())
 
-
-users = Users()
 
 # http://127.0.0.1:8000/users
-app.add_route('/users', users)
+app.add_route('/users', Users())
 
 
 if __name__ == "__main__":
